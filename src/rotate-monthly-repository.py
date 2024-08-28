640fc6308af5748a4f7ac3185f74d1364557d90f
Bret Wortman <bret.wortman@elastic.co>
2024-08-28 08:09:28 -0400

    Update and rename create-monthly-repository to rotate-monthly-repository.py
    Switching to Python before I get too far. Using python's json, request, and argparse libraries will make this a lot easier in the long run.

---
+++ src/rotate-monthly-repository.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+#
+import argparse
+import json
+import request
+
+def create_new_repo():
+    pass
+  
+def unmount_oldest_repo():
+    pass
+
+def update_ilm_policies():
+    pass
+
+def main():
+    pass
+  
+
+if __name__ == "__main__":
+    main()
