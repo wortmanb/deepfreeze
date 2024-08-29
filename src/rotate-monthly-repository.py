bd39b3b78eb06e089bb122c5cdb92807a846f22e
Bret Wortman <bret.wortman@elastic.co>
2024-08-29 09:25:15 -0400

    Update rotate-monthly-repository.py

--- src/rotate-monthly-repository.py
+++ src/rotate-monthly-repository.py
@@ -17,16 +17,18 @@ class Processor:
         repo_name_pattern = config['repo_name_pattern']
         bucket_name_pattern = config['bucket_name_pattern'] \
             if 'bucket_name_pattern' in config else repo_name_pattern
-        # Initialize attributes
-        self.repo_name = repo_name_pattern.format(year=year, month=month)
-        self.bucket_name = bucket_name_pattern.format(year=year, month=month)
+
+        self.repo_name = repo_name_pattern.format(year=config['year'], 
+                                                  month=config['month'])
+        self.bucket_name = bucket_name_pattern.format(year=config['year'],
+                                                      month=config['month'])
         self.policies_url = config['urls']['policies']
 
         repo_list = self.get_repos()
         repo_names = get_repo_names(repo_list)
-        # TODO: Check to ensure the new repo isn't already defined
         if self.repo_name in repo_names:
-            raise Exception(f"Requested repo name {self.repo_name} already exists!")
+            raise Exception(f"Requested repo name {self.repo_name} "
+                            "already exists!")
         self.oldest_repo = repo_list[-1]
         self.newest_repo = repo_list[1]
 
@@ -39,19 +41,24 @@ class Processor:
         pass
 
     def update_ilm_policies(self):
-        rv = requests.get(self.policies_url)
+        policies = json.loads(request.get(self.policies_url))
+        updated_policies = {}
         # Go through these looking for any occurrences of self.newest_repo
         # and change those to use self.repo_name instead.
+
+        # Now, submit the updated policies to _ilm/policy/<policyname>
+        for policy in updated_policies:
+            payload = json.dumps(policy)
         pass
+
+    def get_repos(self) -> list[object]:
+
+        return []
 
     def process(self) -> None:
         self.create_new_repo()
         self.update_ilm_policies()
         self.unmount_oldest_repo()
-
-    def get_repos(self) -> list[object]:
-
-        return []
 
 
 def get_repo_names(dict[]) -> list[str]:
@@ -64,7 +71,7 @@ def get_repo_names(dict[]) -> list[str]:
     return names
 
 
-def main(year: int, month: int, config):
+def main(config):
     Processor(config).process()
     
 
@@ -72,12 +79,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', '-c', dest=config,
                         default='/etc/rotate-monthly-repository.yml',
-                        help='alternate config file (default: /etc/rotate-monthly-repository.yml)')
-    parser.add_argument('year', metavar='YEAR', type=int, help='Year for the new repo')
-    parser.add_argument('month', metavar='MONTH', type=int, help='Month for the new repo')
+                        help='alternate config file (default: /etc/'
+                        'rotate-monthly-repository.yml)')
+    parser.add_argument('year', metavar='YEAR', type=int, 
+                        help='Year for the new repo')
+    parser.add_argument('month', metavar='MONTH', type=int, 
+                        help='Month for the new repo')
     args=parser.parse_args()
 
     with open(configfile, 'r') as file:
         config = yaml.safe_load(file)
+        config['year'] = args.year
+        config['month'] = args.month
     
-    main(args.year, args.month, config)
+    main(config)
