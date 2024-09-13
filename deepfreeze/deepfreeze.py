14abda36fe619a4a05500cd9581163fe0d7177f0
Bret Wortman <bret.wortman@elastic.co>
2024-09-13 08:10:56 -0400

    Freshly black-ified.

--- deepfreeze/deepfreeze.py
+++ deepfreeze/deepfreeze.py
@@ -18,9 +18,24 @@ class Deepfreeze:
     The Deepfreeze is responsible for managing the repository rotation given
     a config file of user-managed options and settings.
     """
-    def __init__(self, year, month, verbose, elasticsearch, ca, username,
-                 password, repo_name_prefix, bucket_name_prefix, style,
-                 base_path, canned_acl, storage_class, keep) -> None:
+
+    def __init__(
+        self,
+        year,
+        month,
+        verbose,
+        elasticsearch,
+        ca,
+        username,
+        password,
+        repo_name_prefix,
+        bucket_name_prefix,
+        style,
+        base_path,
+        canned_acl,
+        storage_class,
+        keep,
+    ) -> None:
         self.year = year
         self.month = month
         self.elasticsearch = elasticsearch
@@ -43,7 +58,7 @@ class Deepfreeze:
         self.es_client = Elasticsearch(
             self.elasticsearch,
             ca_certs=self.ca,
-            basic_auth=(self.username, self.password)
+            basic_auth=(self.username, self.password),
         )
 
         suffix = self.get_next_suffix()
@@ -55,8 +70,9 @@ class Deepfreeze:
         self.latest_repo = self.repo_list[-1]
 
         if self.new_repo_name in self.repo_list:
-            raise Exception(f"Requested repo name {self.new_repo_name} "
-                            "already exists!")
+            raise Exception(
+                f"Requested repo name {self.new_repo_name} " "already exists!"
+            )
 
     def create_new_bucket(self) -> bool:
         """
@@ -66,7 +82,7 @@ class Deepfreeze:
         :rtype:     bool
         """
         try:
-            s3 = boto3.client('s3')
+            s3 = boto3.client("s3")
             s3.create_bucket(Bucket=self.new_bucket_name)
         except ClientError as e:
             logging.error(e)
@@ -84,8 +100,8 @@ class Deepfreeze:
                 "bucket": self.new_bucket_name,
                 "base_path": self.base_path,
                 "canned_acl": self.canned_acl,
-                "storage_class": self.storage_class
-            }
+                "storage_class": self.storage_class,
+            },
         )
 
     def update_ilm_policies(self):
@@ -96,22 +112,30 @@ class Deepfreeze:
         if self.latest_repo == self.new_repo_name:
             logging.warning("Already on the latest repo")
             sys.exit(0)
-        logging.info(f"Attempting to switch from {self.latest_repo} to "
-              f"{self.new_repo_name}")
+        logging.info(
+            f"Attempting to switch from {self.latest_repo} to " f"{self.new_repo_name}"
+        )
         policies = self.es_client.ilm.get_lifecycle()
         updated_policies = {}
         for policy in policies:
             # Go through these looking for any occurrences of self.latest_repo
             # and change those to use self.new_repo_name instead.
-            p = policies[policy]['policy']['phases']
+            p = policies[policy]["policy"]["phases"]
             updated = False
             for phase in p:
-                if 'searchable_snapshot' in p[phase]['actions']:
-                    if p[phase]['actions']['searchable_snapshot']['snapshot_repository'] == self.latest_repo:
-                        p[phase]['actions']['searchable_snapshot']['snapshot_repository'] = self.new_repo_name
+                if "searchable_snapshot" in p[phase]["actions"]:
+                    if (
+                        p[phase]["actions"]["searchable_snapshot"][
+                            "snapshot_repository"
+                        ]
+                        == self.latest_repo
+                    ):
+                        p[phase]["actions"]["searchable_snapshot"][
+                            "snapshot_repository"
+                        ] = self.new_repo_name
                         updated = True
             if updated:
-                updated_policies[policy] = policies[policy]['policy']
+                updated_policies[policy] = policies[policy]["policy"]
 
         # Now, submit the updated policies to _ilm/policy/<policyname>
         if not updated_policies:
@@ -120,10 +144,7 @@ class Deepfreeze:
             logging.info(f"Updating {len(updated_policies.keys())} policies:")
         for pol in updated_policies:
             logging.info(f"\t{pol}")
-            self.es_client.ilm.put_lifecycle(
-                name=pol,
-                policy=updated_policies[pol]
-            )
+            self.es_client.ilm.put_lifecycle(name=pol, policy=updated_policies[pol])
 
     def get_next_suffix(self):
         """
@@ -175,72 +196,121 @@ class Deepfreeze:
 
 
 @click.command()
-@click.argument('year', type=int, required=False,
-                default=datetime.now().year)
-@click.argument('month', type=int, required=False,
-                default=datetime.now().month)
-@click.option('--verbose', '-v', is_flag=True,
-              help='Display extra debugging output during execution')
-@click.option('--elasticsearch', type=str,
-              default=os.environ.get('DEEPFREEZE_ELASTICSEARCH'),
-              required=True,
-              help='URL to use when connecting to elasticsearch (https://elasticsearch.local:9200)')
-@click.option('--ca', type=str,
-              default=os.environ.get('DEEPFREEZE_CA'),
-              required=True,
-              help='path to ca cert file')
-@click.option('--username', type=str,
-              default=os.environ.get('DEEPFREEZE_USERNAME'),
-              required=True,
-              help='username for elasticsearch connection')
+@click.argument("year", type=int, required=False, default=datetime.now().year)
+@click.argument("month", type=int, required=False, default=datetime.now().month)
+@click.option(
+    "--verbose",
+    "-v",
+    is_flag=True,
+    help="Display extra debugging output during execution",
+)
+@click.option(
+    "--elasticsearch",
+    type=str,
+    default=os.environ.get("DEEPFREEZE_ELASTICSEARCH"),
+    required=True,
+    help="URL to use when connecting to elasticsearch (https://elasticsearch.local:9200)",
+)
+@click.option(
+    "--ca",
+    type=str,
+    default=os.environ.get("DEEPFREEZE_CA"),
+    required=True,
+    help="path to ca cert file",
+)
+@click.option(
+    "--username",
+    type=str,
+    default=os.environ.get("DEEPFREEZE_USERNAME"),
+    required=True,
+    help="username for elasticsearch connection",
+)
 @click.password_option(hidden=True)
-@click.option('--repo_name_prefix', type=str,
-              default=os.environ.get('DEEPFREEZE_REPO_NAME_PREFIX'),
-              required=True,
-              help='prefix for naming rotating repositories')
-@click.option('--bucket_name_prefix', type=str,
-              default=os.environ.get('DEEPFREEZE_BUCKET_NAME_PREFIX'),
-              required=True,
-              help='prefix for naming buckets')
-@click.option('--style', type=click.Choice(['oneup', 'monthly']),
-              default=os.environ.get('DEEPFREEZE_STYLE'),
-              required=True,
-              help='suffix can be one-up like indices or date-based (YYYY.MM)')
-@click.option('--base_path', type=str,
-              default=os.environ.get('DEEPFREEZE_BASE_PATH'),
-              required=True,
-              help='base path in the bucket to use for searchable snapshots')
-@click.option('--canned_acl', type=click.Choice([
-                'private',
-                'public-read',
-                'public-read-write',
-                'authenticated-read',
-                'log-delivery-write',
-                'bucket-owner-read',
-                'bucket-owner-full-control'
-                ]),
-              default=os.environ.get('DEEPFREEZE_CANNED_ACL'),
-              required=True,
-              help='Canned ACL as defined by AWS')
-@click.option('--storage_class', type=click.Choice([
-                'standard',
-                'reduced_redundancy',
-                'standard_ia',
-                'intelligent_tiering',
-                'onezone_ia'
-                ]),
-              default=os.environ.get('DEEPFREEZE_STORAGE_CLASS'),
-              required=True,
-              help='Storage class as defined by AWS')
-@click.option('--keep', type=int,
-              default=os.environ.get('DEEPFREEZE_KEEP'),
-              required=True,
-              help='How many repositories should remain mounted?')
-@click.help_option('--help', '-?')
+@click.option(
+    "--repo_name_prefix",
+    type=str,
+    default=os.environ.get("DEEPFREEZE_REPO_NAME_PREFIX"),
+    required=True,
+    help="prefix for naming rotating repositories",
+)
+@click.option(
+    "--bucket_name_prefix",
+    type=str,
+    default=os.environ.get("DEEPFREEZE_BUCKET_NAME_PREFIX"),
+    required=True,
+    help="prefix for naming buckets",
+)
+@click.option(
+    "--style",
+    type=click.Choice(["oneup", "monthly"]),
+    default=os.environ.get("DEEPFREEZE_STYLE"),
+    required=True,
+    help="suffix can be one-up like indices or date-based (YYYY.MM)",
+)
+@click.option(
+    "--base_path",
+    type=str,
+    default=os.environ.get("DEEPFREEZE_BASE_PATH"),
+    required=True,
+    help="base path in the bucket to use for searchable snapshots",
+)
+@click.option(
+    "--canned_acl",
+    type=click.Choice(
+        [
+            "private",
+            "public-read",
+            "public-read-write",
+            "authenticated-read",
+            "log-delivery-write",
+            "bucket-owner-read",
+            "bucket-owner-full-control",
+        ]
+    ),
+    default=os.environ.get("DEEPFREEZE_CANNED_ACL"),
+    required=True,
+    help="Canned ACL as defined by AWS",
+)
+@click.option(
+    "--storage_class",
+    type=click.Choice(
+        [
+            "standard",
+            "reduced_redundancy",
+            "standard_ia",
+            "intelligent_tiering",
+            "onezone_ia",
+        ]
+    ),
+    default=os.environ.get("DEEPFREEZE_STORAGE_CLASS"),
+    required=True,
+    help="Storage class as defined by AWS",
+)
+@click.option(
+    "--keep",
+    type=int,
+    default=os.environ.get("DEEPFREEZE_KEEP"),
+    required=True,
+    help="How many repositories should remain mounted?",
+)
+@click.help_option("--help", "-?")
 @click.version_option()
-def deepfreeze(year, month, verbose, elasticsearch, ca, username, password,
-               repo_name_prefix, bucket_name_prefix, style, base_path,
-               canned_acl, storage_class, keep):
+def deepfreeze(
+    year,
+    month,
+    verbose,
+    elasticsearch,
+    ca,
+    username,
+    password,
+    repo_name_prefix,
+    bucket_name_prefix,
+    style,
+    base_path,
+    canned_acl,
+    storage_class,
+    keep,
+):
     """
     deepfreeze handles creating a new bucket, setting up a repository for
     that bucket, updating ILM policies to use the new bucket, and unmounting
@@ -259,9 +329,22 @@ def deepfreeze(year, month, verbose, elasticsearch, ca, username, password,
     securely provide the password for the elasticsearch username is on the
     command line using secure entry, and so that's what we do.
     """
-    Deepfreeze(year, month, verbose, elasticsearch, ca, username, password,
-               repo_name_prefix, bucket_name_prefix, style, base_path,
-               canned_acl, storage_class, keep).rotate()
+    Deepfreeze(
+        year,
+        month,
+        verbose,
+        elasticsearch,
+        ca,
+        username,
+        password,
+        repo_name_prefix,
+        bucket_name_prefix,
+        style,
+        base_path,
+        canned_acl,
+        storage_class,
+        keep,
+    ).rotate()
 
 
 if __name__ == "__main__":
